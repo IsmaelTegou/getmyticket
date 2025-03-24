44a7a8c29843b3b5ee70156294a4ab19b50d29ee
ismaelTegou <ismaeltegou7@gmail.com>
2025-03-24 14:59:18 +0100

    feature/#1-Configuration du depot GitHub

---
+++ getmyticket-backend/src/main/java/kag/glo4/getmyticket_backend/GetmyticketBackendApplication.java
@@ -0,0 +1,13 @@
+package kag.glo4.getmyticket_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetmyticketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetmyticketBackendApplication.class, args);
+	}
+
+}
---
+++ getmyticket-backend/src/test/java/kag/glo4/getmyticket_backend/GetmyticketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kag.glo4.getmyticket_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetmyticketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
