864e6745a88d9c6abca25f17ddb92e7215f8518f
ismaelTegou <ismaeltegou7@gmail.com>
2025-04-06 13:18:51 +0200

    creation de l'interface de connexion et d'inscription terminee

--- getmyticket-frontend/src/app/app.routes.ts
+++ getmyticket-frontend/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {LayoutComponent} from './components/layout/layout.component';
+import {HomeComponent} from './components/layout/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path:'',
+    component: LayoutComponent,
+  },
+  {
+    path:'',
+    component:LayoutComponent,
+    children:[
+      {
+        path:'home',
+        component: HomeComponent
+      }
+    ]
+  }
+];
